f2ff5685566aa11ccf617f34bec15fff2c31ecc7
waldekm999 <waldekm999@hotmail.com>
2024-02-18 14:53:55 +0100

    accounts done

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+
+"""
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'nazwa użytkownika', 'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'hasło', 'class': 'form-control'}))
+"""
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'nazwa użytkownika', 'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'adres email', 'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'placeholder': '<PASSWORD>', 'class': 'form-control'}),
+        }
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są takie same')
+        return cd['password2']
---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html',
+                  {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password1'])
+            new_user.save()
+            return render(request, 'accounts/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'user_form': user_form})
+
